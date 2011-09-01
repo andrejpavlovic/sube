cb2f01a09940eefb4c2c7fca76c187a7757e53ab
Andrej <Andrej@38686702-15cf-42e4-a595-3071df8bf5ea>
2011-09-01 02:06:51 +0000

    #1036: Added some more faqs with regards to spam.

--- content/contact.php
+++ content/contact.php
@@ -5,23 +5,8 @@ require('top.php');
 ?>
 
 <div id="content" style="background-image:url(images/bg_contact.jpg);">
-<h1>Contact Information</h1>
 
-Please use the email below if:
-<ul>
-<li>you need help</li>
-<li>want to give feedback</li>
-<li>wish to advertize on our site</li>
-<li>want to help with the site</li>
-</ul>
-
-The email is:
-<table>
-<tr><td>info</td><td>at</td><td>uwsube</td><td>dot</td><td>com</td></tr>
-</table>
-
-
-<h1 style="padding-top:20px;">Frequently Asked Questions</h1>
+<h1>Frequently Asked Questions</h1>
 
 <dl class="faq">
 	<dt>How do I create an account?</dt>
@@ -35,7 +20,30 @@ The email is:
 	<dd>
 		Try to <a href="<?php echo $_SERVER['PHP_SELF']?>?content=my&type=pass">reset your password</a>.
 	</dd>
+	
+	<dt>An email that I received looks suspicious, what should I do?</dt>
+	<dd>
+		Don't do anything, just ignore it. Please <strong>do not</strong> notify us about the problem.
+	</dd>
 </dl>
+
+
+<h1 style="padding-top:20px;">Contact Information</h1>
+
+Please use the email below if:
+<ul>
+	<li>you need help</li>
+	<li>want to give feedback</li>
+	<li>wish to advertize on our site</li>
+	<li>want to help with the site</li>
+</ul>
+
+The email is:
+<table>
+<tr><td>info</td><td>at</td><td>uwsube</td><td>dot</td><td>com</td></tr>
+</table>
+
+<p style="padding-top:10px;">NOTE: If you don't receive a response within 1-2 days, it means we have read your email, and decided not to act on it.</p>
 
 
 <h1 style="padding-top:20px;">Banners</h1>
