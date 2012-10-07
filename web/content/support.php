38211ab0b0c793a3501ffc3dde51065e7f7e37c0
Andrej Pavlovic <andrej.pavlovic@pokret.org>
2012-10-07 16:41:42 -0400

    Added missing support page.

---
+++ web/content/support.php
@@ -0,0 +1,27 @@
+<?php
+require('include/global_data.php');
+$html_title = 'Our Supporters';
+require('top.php');
+?>
+
+<div id="content" style="background-image:url(images/bg_about.jpg);">
+
+<h1>Our Supporters</h1>
+
+<p>The following organizations are helping us spread the word about UWSUBE. Thank you very much for supporting UW students and our website.
+
+<ul>
+<li><a href="http://www.uwmusic.org/">UW Music Club</a></li>
+<li><a href="http://www.uwssa.org/">UW Serbian Students Association</a></li>
+</ul>
+
+<h1>Banners</h1>
+<p>If you or your organization would like to help, please inform your friends and members. There are a few banners at the bottom of this page which you can use to link to our website.</p>
+<img src="images/banners/uwsube1.gif" style="float:left;margin:0 0 10px 50px;width:204px;height:166px" alt="Visit UW Students Buying Exchange Now!" />
+<img src="images/banners/uwsube2w.jpg" style="float:right;margin:25px 80px 0 0;width:150px;height:40px" alt="UW Students Underground Buying Exchange" />
+<img src="images/banners/uwsube2b.jpg" style="float:right;margin:30px 80px 0 0;width:150px;height:40px" alt="UW Students Underground Buying Exchange" />
+<div class="clear"></div>
+
+</div>
+
+<?php require('bottom.php'); ?>
