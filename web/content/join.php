42f89a7ae24c4adfc8db157616975cd238645896
Andrej Pavlovic <andrej.pavlovic@pokret.org>
2012-10-07 20:02:33 -0400

    Join section now links to github.

--- web/content/join.php
+++ web/content/join.php
@@ -9,14 +9,10 @@ require('top.php');
 <div id="content" style="background-image:url(images/bg_join.jpg);">
 <h1>Join Us</h1>
 
-<p>We are looking for volunteers to <em>take over</em> this project. While it is nice to have people help out a bit every now and then, we have moved on and need some one to actually take full charge of the project. Until this happens there won't be much further progress made with UWSUBE. If you are interested, please visit the 'Contact Us' page and send us an email. All you need is to have ambition and knowledge in at least one of the areas listed below.</p>
+<p>We are looking for volunteers to <em>take over</em> this project. While it is nice to have people help out a bit every now and then, we have moved on and need some one to actually take full charge of the project. Until this happens there won't be much further progress made with UWSUBE. If you are interested, please visit the 'Contact Us' page and send us an email. A little ambition can go a long way and we are here to give you the support you need to get things started.</p>
 
-<p><strong>Marketing Director</strong><br />
-A Marketing Director is tasked with promotion and marketing of UWSUBE within the university as well as sponsor relations. Promotion within the university can be achieved via posters, printed article in school newsletters, links on university webpages, emailing class lists, and any other creative means.</p>
-<p><strong>Web Analyst</strong><br />
-A Web Analyst is tasked with the web design and maintenance of UWSUBE. Roles and responsibilities can include bug fixing, feature enhancement and design, web traffic monitoring, and content management. The web analyst should know or be willing to learn PHP and MySQL.</p>
-<p><strong>Graphic Artist</strong><br />
-The roles and responsibilities of a Graphic Artist can include logo design, banner design, poster design, and other web, computer, and print based deliverables. The graphic artist should know or be willing to learn Adobe Photoshop (or an equivalent graphics program) and basic HTML.</p>
+<h1>GitHub</h1>
+<p>As of October 2012 we have made the source code of this site feely available on <a href="https://github.com/andrejpavlovic/sube" target="_blank">GitHub</a>. You can now contribute your own ideas or build a site just like this one!</p>
 
 </div>
 
