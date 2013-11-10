0902b11fea087f7394b6b190fa4e791ea71a1c36
Andrej Pavlovic <andrej.pavlovic@pokret.org>
2013-11-10 18:41:13 -0500

    Removed join us paragraph. Moved banners section.

--- web/content/contact.php
+++ web/content/contact.php
@@ -45,15 +45,6 @@ The email is:
 
 <p style="padding-top:10px;">NOTE: Please ask a friend for help before emailing us if possible. This is a pro bono service and answering individual questions can be very taxing on our resources.</p>
 
-
-<h1 style="padding-top:20px;">Banners</h1>
-<p>If you or your organization would like to help, please inform your friends and members. There are a few banners below which you can use to link to our website.</p>
-<img src="images/banners/uwsube1.gif" style="float:left;margin:0 0 10px 50px;width:204px;height:166px" alt="Visit UW Students Underground Book Exchange Now!" />
-<img src="images/banners/uwsube2w.jpg" style="float:right;margin:25px 80px 0 0;width:150px;height:40px" alt="UW Students Underground Book Exchange" />
-<img src="images/banners/uwsube2b.jpg" style="float:right;margin:30px 80px 0 0;width:150px;height:40px" alt="UW Students Underground Book Exchange" />
-<div class="clear"></div>
-
-
 </div>
 
 <?php
--- web/content/join.php
+++ web/content/join.php
@@ -7,12 +7,17 @@ require('top.php');
 ?>
 
 <div id="content" style="background-image:url(images/bg_join.jpg);">
-<h1>Join Us</h1>
-
-<p>We are looking for volunteers to <em>take over</em> this project. While it is nice to have people help out a bit every now and then, we have moved on and need some one to actually take full charge of the project. Until this happens there won't be much further progress made with UWSUBE. If you are interested, please visit the 'Contact Us' page and send us an email. A little ambition can go a long way and we are here to give you the support you need to get things started.</p>
-
 <h1>GitHub</h1>
+
 <p>As of October 2012 we have made the source code of this site feely available on <a href="https://github.com/andrejpavlovic/sube" target="_blank">GitHub</a>. You can now contribute your own ideas or build a site just like this one!</p>
+
+
+<h1 style="padding-top:20px;">Banners</h1>
+<p>If you or your organization would like to help, please inform your friends and members. There are a few banners below which you can use to link to our website.</p>
+<img src="images/banners/uwsube1.gif" style="float:left;margin:0 0 10px 50px;width:204px;height:166px" alt="Visit UW Students Underground Book Exchange Now!" />
+<img src="images/banners/uwsube2w.jpg" style="float:right;margin:25px 80px 0 0;width:150px;height:40px" alt="UW Students Underground Book Exchange" />
+<img src="images/banners/uwsube2b.jpg" style="float:right;margin:30px 80px 0 0;width:150px;height:40px" alt="UW Students Underground Book Exchange" />
+<div class="clear"></div>
 
 </div>
 
